828ccc7bcc985b4896d44b7aa86dee257e61998d
Yolanda McCarthy <yolandamccarthy@Yolandas-MacBook-Air.local>
2018-02-26 18:43:33 -0700

    fixing connection.js file

--- config/connection.js
+++ config/connection.js
@@ -1,8 +1,18 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  port: 3306,
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
+
+if(process.env.JAWSDB_URL) {
+  connection=mysql.createConnection(process.env>JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   password: "",
@@ -17,6 +27,8 @@ connection.connect(function(err) {
   }
   console.log("connected as id " + connection.threadId);
 });
+
+};
 
 // Export connection for our ORM to use.
 module.exports = connection;
