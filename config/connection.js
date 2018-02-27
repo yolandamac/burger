fc61d540433230b187f682ed14349f76dc0f8b16
Yolanda McCarthy <yolandamccarthy@Yolandas-MacBook-Air.local>
2018-02-26 20:16:00 -0700

    final code edit

--- config/connection.js
+++ config/connection.js
@@ -1,14 +1,6 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "burgers_db"
-// });
-
 if(process.env.JAWSDB_URL) {
   connection=mysql.createConnection(process.env.JAWSDB_URL);
 } else {
